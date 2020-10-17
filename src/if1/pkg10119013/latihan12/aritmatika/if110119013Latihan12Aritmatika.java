945759fff1362d8d7aa2d44eef6b00b874c2fe3a
FionaAvila <fionaavila15@gmail.com>
2020-10-17 17:40:17 +0700

    Initial Commit

---
+++ src/if1/pkg10119013/latihan12/aritmatika/if110119013Latihan12Aritmatika.java
@@ -0,0 +1,38 @@
+/*
+ * NAMA  : Fiona Avila Putri
+ * NIM   : 10119013
+ * KELAS : IF1 2019/2020 (PBO1)
+ */
+
+
+
+package if1.pkg10119013.latihan12.aritmatika;
+
+/**
+ *
+ * @author Fiona Avila
+ */
+
+
+public class if110119013Latihan12Aritmatika {
+
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (b--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+    }
+    
+}
